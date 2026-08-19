dbc347fa6e2e2bcd783b2a8eb99a5b1aa1bf1a06
Randhomme <randormi@gmail.com>
2026-08-19 14:11:03 +0200

    feat: add ctor and ToString method

--- TPToolkitLib/Mesh/Classes/MdbMaterial.cs
+++ TPToolkitLib/Mesh/Classes/MdbMaterial.cs
@@ -4,5 +4,21 @@
     {
         public string MaterialName { get; set; }
         public string TextureName { get; set; }
+
+        public MdbMaterial()
+        {
+            MaterialName = TextureName = string.Empty;
+        }
+
+        public MdbMaterial(string materialName, string textureName)
+        {
+            MaterialName = materialName;
+            TextureName = textureName;
+        }
+
+        public override string ToString()
+        {
+            return MaterialName;
+        }
     }
 }
