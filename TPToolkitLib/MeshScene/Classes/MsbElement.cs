bd90cbfb4e5acc97d3db38014480f7167c6c309d
Randhomme <randormi@gmail.com>
2026-08-24 19:01:39 +0200

    feat: add parent element

--- TPToolkitLib/MeshScene/Classes/MsbElement.cs
+++ TPToolkitLib/MeshScene/Classes/MsbElement.cs
@@ -5,6 +5,7 @@ namespace TPToolkitLib.MeshScene.Classes
 {
     public class MsbElement : MsbNamedElement
     {
+        public MsbElement? Parent { get; set; }
         public Vector3 Pivot { get; set; }
         public Vector3 Position { get; set; }
         public Vector3 Rotation { get; set; } // yaw, pitch, roll (in radian)
